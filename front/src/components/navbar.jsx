282bb96313847149cc2a6162e5fc5d78f7e3122c
Antoine Schmerber-Perraud <antoine.schmerber-perraud@edu.devinci.fr>
2024-03-26 14:01:15 +0100

    :lipstick: Modification Css

--- front/src/components/navbar.jsx
+++ front/src/components/navbar.jsx
@@ -1,24 +1,35 @@
-import { useState } from 'react'
-import styles from '../styles/navbar.module.css';
+import { useState } from "react";
+import styles from "../styles/navbar.module.css";
 
 const Navbar = () => {
-    
-    
     return (
         <div className={styles.navbar}>
             <div className={styles.btnHome}>
-                <a href=""><img src="" alt="" /></a>
+                <a href="">
+                    <img src="./src/assets/Logo.svg" alt="" />
+                </a>
             </div>
             <ul>
-                <li><a href="">Accueil</a></li>
-                <li><a href="">Projets</a></li>
-                <li><a href="">Soumissions Projets</a></li>
+                <li>
+                    <a href="">Accueil</a>
+                </li>
+                <li>
+                    <a href="">Projets</a>
+                </li>
+                <li>
+                    <a href="">Soumissions Projets</a>
+                </li>
             </ul>
-            <div className={styles.btnProfil}>
-                <a href=""><img src="" alt="" /></a>
+            <div className={styles.connecter}>
+                <img
+                    className={styles.img}
+                     src="./src/assets/recherche.svg"
+                    alt=""
+                />
+                <button className={styles.connexion}>Se connecter</button>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
