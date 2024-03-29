38c0e0da3b14e851524d9f332770e71dfee8bfd6
Wolap <wolap.wolap@gmail.com>
2024-03-29 16:20:39 +0100

    :sparkles: ADD display user's liked projects

--- back/controllers/UserController.js
+++ back/controllers/UserController.js
@@ -3,6 +3,27 @@ import jwt from 'jsonwebtoken'
 
 const prisma = new PrismaClient()
 
+// const getProfile = async (req, res) => {
+//     const token = req.headers["x-access-token"]
+//     const decodedToken = jwt.decode(token, "nodejsisawesome")
+
+//     if (!token) {
+//         return res.json({ error: "No token provided!" })
+//     }
+
+//     const user = await prisma.user.findUnique({
+//         where: {
+//             id: decodedToken.id
+//         }
+//     }) .then(user => {
+//         res.json(user)
+//     })
+//     .catch(error => {
+//         res.json(error)
+//     })
+// }
+
+// get profile with the like of the user
 const getProfile = async (req, res) => {
     const token = req.headers["x-access-token"]
     const decodedToken = jwt.decode(token, "nodejsisawesome")
@@ -14,6 +35,9 @@ const getProfile = async (req, res) => {
     const user = await prisma.user.findUnique({
         where: {
             id: decodedToken.id
+        },
+        include: {
+            likes: true
         }
     }) .then(user => {
         res.json(user)
--- front/src/pages/profil.jsx
+++ front/src/pages/profil.jsx
@@ -6,23 +6,15 @@ import stylesProjets from '../styles/projetSoumis.module.css';
 
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+import ProjectCard from '../components/projectCard';
 import defaultProjectImage from '../assets/bgForm.png';
 
 const Profil = () => {
 
     const [infoUser, setInfoUser] = useState([]);
     const [infoProjet, setInfoProjet] = useState([]); 
-    const [likesByProject, setLikesByProject] = useState({});
     const token = localStorage.getItem('token');
     const userId = localStorage.getItem('userId');
-
-    useEffect(() => {
-        // récup likes du localStorage au premier rendu
-        const likesDictionary = localStorage.getItem('likesByProject');
-        if (likesDictionary) {
-            setLikesByProject(JSON.parse(likesDictionary));
-        }
-    }, []);
 
     // fetch user info
     useEffect(() => {    
@@ -46,6 +38,7 @@ const Profil = () => {
         .then(response => response.json())
         .then(data => {
             setInfoUser(data)
+            console.log("data", data)
         }
         );     
     }
@@ -71,55 +64,12 @@ const Profil = () => {
                         image: `data:image/jpeg;base64,${base64Image}`,
                     };
                 });
-
                 setInfoProjet(parsedData);
             })
             .catch((error) => {
                 console.error('Error fetching data:', error);
             });
     }, []);
-
-    function likeProject(projectId) {
-        const key = projectId.toString();
-        const userId = localStorage.getItem('userId');
-    
-        // vérif' si utilisateur connecté
-        if (!userId) {
-            return false;
-        }
-    
-        // maj likes
-        // méthode de useState, prend fonction en argument
-        setLikesByProject(prevLikes => {
-            // copie de l'ancienne valeur
-            const updatedLikes = { ...prevLikes };
-            
-            // Vérifier si le projet existe dans le dictionnaire
-            if (!updatedLikes[key]) {
-                // Si le projet n'existe pas, créer un nouveau dictionnaire avec l'utilisateur
-                return { ...updatedLikes, [key]: { [userId]: true } };
-            } else {
-                // Si le projet existe déjà
-                if (updatedLikes[key][userId]) {
-                    // Si l'utilisateur a déjà aimé, supprimer le like
-                    const { [userId]: deletedValue, ...remainingLikes } = updatedLikes[key];
-                    return { ...updatedLikes, [key]: remainingLikes };
-                } else {
-                    // Si l'utilisateur n'a pas aimé, ajouter le like
-                    return { ...updatedLikes, [key]: { ...updatedLikes[key], [userId]: true } };
-                }
-            }
-        });  
-        console.log('likesByProject', likesByProject)
-        return true;
-    }
-
-    // Récupérer le nombre de likes
-    function getLikesCount(projectId) {
-        const key = projectId.toString();
-        return likesByProject[key] ? Object.keys(likesByProject[key]).length : 0;
-    }
-
 
     return (
         <>
@@ -148,34 +98,17 @@ const Profil = () => {
                     <h2>Mes projets</h2>
                     {infoProjet.filter(item => item.userId == userId).map((item, index) => (
                         
-                        <div key={index} className={stylesProjets.card}>
-                            <img
-                                className={stylesProjets.image}
-                                src={item.image ?? defaultProjectImage}
-                                alt=""
-                            />
-                            <h2 className={stylesProjets.titleProject}> {item.nomProjet} </h2>
-                            <h3 className={stylesProjets.lieu}> {item.lieu} </h3>
-                            <p className={stylesProjets.description}> {item.description} </p>
-                            <h3 className={stylesProjets.budget}> Budget : {item.budget} </h3>
-                            <p className={stylesProjets.categorie}> {item.categorie} </p>
-                            <div>
-                                <button
-                                    className={stylesProjets.likeButton}
-                                    onClick={() => likeProject(item.id)}
-                                >
-                                    Like
-                                </button>
-                                <span className={stylesProjets.likeCount}>
-                                    {getLikesCount(item.id)}
-                                </span>
-                            </div>
-                        </div>
+                        <ProjectCard key={index} item={item} />
                     ))}
                 </div>
 
                 <div>
-                    <h2>Mes coups de coeurs</h2>
+                    <h2>Mes coups de coeurs</h2>                    
+                    
+                    {infoProjet.filter(item => item.likes.some((like) => like.userId == userId)).map((item, index) => (
+                        <ProjectCard key={index} item={item} />
+                    ))}
+                    
                 </div>
             </section>
             <Footer />
