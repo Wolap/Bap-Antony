81450cbb2392843b2fc53527d8ca3e6e01e0c44e
Wolap <wolap.wolap@gmail.com>
2024-03-29 09:16:03 +0100

    :construction: WIP

--- front/src/pages/profil.jsx
+++ front/src/pages/profil.jsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
+import {Buffer} from 'buffer';  
 import styles from '../styles/profil.module.css';
 
 import Navbar from '../components/navbar';
@@ -8,6 +9,7 @@ import Footer from '../components/footer';
 const Profil = () => {
 
     const [infoUser, setInfoUser] = useState([]);
+    const [infoProjet, setInfoProjet] = useState([]); 
     const [likesByProject, setLikesByProject] = useState({});
     const token = localStorage.getItem('token');
 
@@ -19,7 +21,7 @@ const Profil = () => {
         }
     }, []);
 
-    // test du chat 
+
     useEffect(() => {    
         console.log('token', token);
         console.log('useEffect running')
@@ -46,9 +48,37 @@ const Profil = () => {
             setInfoUser(data)
             console.log('data : ',data)
         }
-        );
-        
+        );     
     }
+
+    // recup projet soumis mais sans where 
+    useEffect(() => {
+        fetch("http://localhost:3000/soumissions")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }   
+                return response.json();
+            })
+            .then((payload) => {
+                const parsedData = payload.map((item) => {
+                    if (item.image.data.length === 0) {
+                        return { ...item, image: undefined };
+                    }
+
+                    const base64Image = Buffer.from(item.image.data).toString('base64');
+                    return {
+                        ...item,
+                        image: `data:image/jpeg;base64,${base64Image}`,
+                    };
+                });
+
+                setInfoProjet(parsedData);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
 
 
     return (
